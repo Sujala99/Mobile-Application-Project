fc2a2c6be12bb96f2c87da2978904e835d4e1138
Sujala99 <sujala.rai2004@gmail.com>
2025-03-06 20:11:43 +0545

    fixing errors

--- lib/features/appointment/presentation/view/appointment_view.dart
+++ lib/features/appointment/presentation/view/appointment_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_application_project/features/doctor/presentation/view/doctor_view.dart';
+import 'package:mobile_application_project/features/home/presentation/view/profile_view.dart';
+import 'package:mobile_application_project/features/home/presentation/view_model/home_cubit.dart';
+import 'package:mobile_application_project/features/home/presentation/view_model/home_state.dart';
 
 class AppointmentView extends StatelessWidget {
   const AppointmentView({super.key});
@@ -16,25 +21,56 @@ class AppointmentView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch, // Make the column fill the width
           children: <Widget>[
             _buildAppointmentCard(
-              doctorName: 'Dr. Missy Smith',
-              date: '2/25/2025',
-              time: '17:56',
+              doctorName: 'Dr. Sabina Maharjan',
+              date: '3/04/2025',
+              time: '17:00',
               status: 'Pending',
             ),
             _buildAppointmentCard(
-              doctorName: 'Dr. John Doe',
+              doctorName: 'Dr. Pratime Khatri',
               date: '3/10/2025',
               time: '14:30',
               status: 'Accepted',
             ),
             _buildAppointmentCard(
-              doctorName: 'Dr. Alex Turner',
+              doctorName: 'Dr. Missy Smith',
               date: '4/5/2025',
               time: '10:00',
               status: 'Rejected',
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          return BottomNavigationBar(
+            backgroundColor: Colors.deepPurple,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.dashboard),
+                label: 'Dashboard',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Doctor',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.group),
+                label: 'Appointment',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: 'Account',
+              ),
+            ],
+            currentIndex: state.selectedIndex,
+            selectedItemColor: Colors.white,
+            onTap: (index) {
+              context.read<HomeCubit>().onTabTapped(index);
+              _onBottomNavTap(context, index);
+            },
+          );
+        },
       ),
     );
   }
@@ -91,6 +127,37 @@ class AppointmentView extends StatelessWidget {
         return Colors.red;
       default:
         return Colors.black;
+    }
+  }
+
+  void _onBottomNavTap(BuildContext context, int index) {
+    switch (index) {
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const DoctorView(),
+          ),
+        );
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const AppointmentView(),
+          ),
+        );
+        break;
+      case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProfilePage(),
+          ),
+        );
+        break;
+      default:
+        break;
     }
   }
 }
